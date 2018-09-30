24141db2b7736307d060619f17ca6a81749f0874
Alexey Nevgin <nevgin@yandex.ru>
2018-09-30 08:13:19 +0000

    Add change

--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -f /etc/nginx/sites-enabled/default.conf
-sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
-sudo /etc/init.d/mysql start
+cd /home/box/web/ask
+sudo killall gunicorn
+gunicorn -b 0.0.0.0:8000 ask.wsgi:application -D
+#sudo /etc/init.d/gunicorn stop
+#sudo /etc/init.d/mysql start
+sudo /etc/init.d/nginx restart
